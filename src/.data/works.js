c8f401cf8dee151ff65e74241a6dba3930513e45
Nannita1403 <nannimagi@gmail.com>
2024-04-03 12:07:05 +0200

    ok

---
+++ src/.data/works.js
@@ -0,0 +1,42 @@
+export const Works = [
+    {
+      title: "Ejercitacion Color 1: Estructura",
+      description: 
+      "Primer proyecto en Rock The Code, donde se pone en práctica todo lo aprendido de HTML5 así como de CSS3, haciendo uso de `positions`, `flex`, `custom variables`, etc.",
+      github: `https://github.com/Nannita1403/color-flipper`,
+      netlify: `https://ej-colorflipper.netlify.app/`,
+      preview: {src:'/'},
+      },
+      {
+        title: "Proyecto 1: Estructura y estilo",
+        description: 
+        "Primer proyecto en Rock The Code, donde se pone en práctica todo lo aprendido de HTML5 así como de CSS3, haciendo uso de `positions`, `flex`, `custom variables`, etc.",
+        github: `https://github.com/Nannita1403/PROY-1-Landing-Apple`,
+        netlify: `https://p1-rtc-landing.netlify.app/`,
+        preview: {src:'/'},
+      },
+      {
+        title: "Proyecto 2: Filtrado y Tienda Online de Comercio",
+        description: 
+        "Con este proyecto se abre un conjunto de proyectos destinados a practicar Javascript, repasando HTML y CSS. Para el mismo, se crea una serie de productos para, posteriormente, poder filtrar por vendedor y por precio. Todo ello creado mediante JS.",
+        github: `https://github.com/Nannita1403/p2-NIKE`,
+        netlify: `https://p2-nike-rtc.netlify.app/`,
+        preview: {src:'/icon-html.png'},
+      },
+      {
+        title: "Proyecto 3: 'Inspiration'",
+        description: 
+        "Con este proyecto, de parecido razonable a Pinterest, se practica la conexión con APIs a través de Javascript, en este caso la API Unsplash.",
+        github: `https://github.com/Nannita1403/PROY3-RTC`,
+        netlify: `https://ej-colorflipper.netlify.app/`,
+        preview: {src:'/icon-html.png'},
+      },
+      {
+        title: "Proyecto 1: Estructura y estilo",
+        description: 
+        "Primer proyecto en Rock The Code, donde se pone en práctica todo lo aprendido de HTML5 así como de CSS3, haciendo uso de `positions`, `flex`, `custom variables`, etc.",
+        github: `https://github.com/Nannita1403/color-flipper`,
+        netlify: `https://ej-colorflipper.netlify.app/`,
+        preview: {src:'/icon-html.png'},
+      }
+];
--- src/components/4.footer/sectionFooter.js
+++ src/components/4.footer/sectionFooter.js
@@ -3,11 +3,12 @@ export const footer = document.createElement("footer");
 import "./footer.css";
 
 footer.innerHTML += `
+  <p>©️ 2024 All rights reserved</p> 
   <div class="footer-links">
         <img src="/Instagram.png" alt="Instagram">
         <img src="/Discord.png" alt="Discord">
         <img src="/Github.png" alt="GitHub">
-    </div>
-    <p>©️ Design by Natalia Magi</p>
+  </div>
+  <p>Design By Natalia Magi</p>
   </footer>
   `;
